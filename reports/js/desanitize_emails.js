f487f3a448cddfccacb6355238f2e752a242fc71
Thomas Farr <thomas@xtansia.com>
2016-04-14 19:29:10 +1200

    Fix email desanitization

--- reports/js/desanitize_emails.js
+++ reports/js/desanitize_emails.js
@@ -1,7 +1,7 @@
 $('a').each(function() {
     if (this.href.substring(0, 7) == 'mailto:') {
         this.href = this.href.replace(/\(at\)/g, '@').replace(/\(dot\)/g, '.');
-        if (this.text.indexOf('{at}') > -1) {
+        if (this.text.indexOf('(at)') > -1) {
             this.text = this.text.replace(/\(at\)/g, '@').replace(/\(dot\)/g, '.');
         }
     }
