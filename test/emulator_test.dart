6467cdb01d6b618ec9537477bed95e36f1a6fb47
Thomas Farr <xtansia@xtansia.com>
2016-07-25 23:26:36 +1200

    Add simple pattern test for getTime method

--- test/emulator_test.dart
+++ test/emulator_test.dart
@@ -1,9 +1,39 @@
 @TestOn('browser')
 
+import 'dart:html';
 import 'package:test/test.dart';
 
 import 'package:smartwatch/emulator.dart';
 
+class TestApp implements EmulatorApplication {
+  @override
+  void destroy() {
+    // TODO: implement destroy
+  }
+
+  @override
+  void init(Emulator emulator) {
+    // TODO: implement init
+  }
+
+  @override
+  void render() {
+    // TODO: implement render
+  }
+
+  @override
+  void update() {
+    // TODO: implement update
+  }
+}
+
 void main () {
+  Emulator emulator = new Emulator(querySelector('#emulator'), 320, 320, new TestApp());
+
+  group('getTime()', () {
+    test('result is in form \'HH:MM:SS (AM|PM)\'', () {
+      expect(emulator.getTime(), matches(r"^\d\d:\d\d:\d\d (?:AM|PM)$"));
+    });
+  });
   // TODO: implement testing
 }
