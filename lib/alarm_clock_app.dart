9e672c20360df2e6d7beb68a4f0f9e929c938bed
Thomas Farr <xtansia@xtansia.com>
2016-07-25 23:44:06 +1200

    Beginnings of AlarmClockApp, currently only renders time in center of screen

---
+++ lib/alarm_clock_app.dart
@@ -0,0 +1,25 @@
+import 'package:smartwatch/emulator.dart';
+
+class AlarmClockApp implements EmulatorApplication {
+  Emulator _emulator;
+
+  @override
+  void init(Emulator emulator) {
+    _emulator = emulator;
+  }
+
+  @override
+  void update() {
+		// TODO: implement update
+  }
+
+  @override
+  void render() {
+    _emulator.screen.drawTextCentered(_emulator.getTime(), _emulator.screen.width / 2, _emulator.screen.height / 2);
+  }
+
+  @override
+  void destroy() {
+    // TODO: implement destroy
+  }
+}
