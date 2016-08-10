400ea68c5df47c3c3b727532fb1e1a1f79c943d2
Thomas Farr <xtansia@xtansia.com>
2016-08-11 00:54:11 +1200

    Remove reference to hello_test

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,9 +1,7 @@
 import 'package:test/test.dart';
 
-import 'hello_test.dart' as hello;
 import 'emulator_test.dart' as emulator;
 
 void main() {
-  group('hello', hello.main);
   group('emulator', emulator.main);
 }
