de31f0451f54277551805680cb9682e323f7e332
Thomas Farr <xtansia@xtansia.com>
2016-08-15 03:29:28 +1200

    Add buttons to app

--- lib/alarm_clock_app.dart
+++ lib/alarm_clock_app.dart
@@ -7,12 +7,11 @@ class AlarmClockApp implements EmulatorApplication {
   void init(Emulator emulator) {
     _emulator = emulator;
     _emulator.screen.font = '16px Arimo';
+    _emulator.screen.onTap.listen(_onTap);
   }
 
   @override
-  void update(num delta) {
-
-  }
+  void update(num delta) {}
 
   @override
   void render() {
@@ -22,8 +21,32 @@ class AlarmClockApp implements EmulatorApplication {
     _emulator.screen.drawText(_emulator.getTime(), width / 2, height / 2 + 12,
         align: 'center');
 
-    _emulator.screen.drawRectInnerStroke(0, height - 40, width / 2, 40);
-    _emulator.screen.drawRectInnerStroke(width / 2, height - 40, width / 2, 40);
+    _drawButton('Stopwatch Mode', 0, 0, width / 2, 40);
+    _drawButton('Customise', 0, height - 40, width / 2, 40);
+    _drawButton('Set Alarm', width / 2, height - 40, width / 2, 40);
+  }
+
+  void _drawButton(String text, num x, num y, num w, num h) {
+    _emulator.screen
+        .drawRectWithInnerStroke(x, y, w, h, fillColour: 'rgba(0, 0, 0, 0.25)');
+    _emulator.screen.drawText(text, x + w / 2, y + h / 2 + 4, align: 'center');
+  }
+
+  void _onTap(TapEvent e) {
+    if (e.y < 40) {
+      if (e.x < _emulator.screen.width / 2) {
+        // TODO: Implement Stopwatch Mode
+        print('Stopwatch Mode');
+      }
+    } else if (e.y > _emulator.screen.height - 40) {
+      if (e.x < _emulator.screen.width / 2) {
+        // TODO: Implement Customise
+        print('Customise');
+      } else {
+        // TODO: Implement Set Alarm
+        print('Set Alarm');
+      }
+    }
   }
 
   @override
