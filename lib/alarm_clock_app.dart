81f4d84ec98cce23d0b3bcdee4d83c771a879001
Thomas Farr <xtansia@xtansia.com>
2016-08-15 02:44:46 +1200

    Remove experiment stuff from app

--- lib/alarm_clock_app.dart
+++ lib/alarm_clock_app.dart
@@ -2,35 +2,16 @@ import 'package:smartwatch/emulator.dart';
 
 class AlarmClockApp implements EmulatorApplication {
   Emulator _emulator;
-  String _notifyText;
-  int _notifyTimer = 0;
 
   @override
   void init(Emulator emulator) {
     _emulator = emulator;
     _emulator.screen.font = '16px Arimo';
-    _emulator.screen.bgStyle = 'cyan';
-    _emulator.screen.onTap
-        .listen((pos) => _notify('tapped @ (x=${pos.x}, y=${pos.y})'));
-    _emulator.screen.onSwipe.listen((dir) => _notify('swiped $dir'));
-    _emulator.screen.onSwipe.listen((dir) => _emulator.speaker.setVolume(
-        _emulator.speaker.getVolume() +
-            (dir == SwipeDirection.UP
-                ? 10
-                : dir == SwipeDirection.DOWN ? -10 : 0)));
-  }
-
-  void _notify(String text) {
-    _emulator.speaker.playSound('kick_drum');
-    _notifyText = text;
-    _notifyTimer = 500;
   }
 
   @override
   void update(num delta) {
-    if (_notifyTimer > 0) {
-      _notifyTimer -= delta;
-    }
+
   }
 
   @override
@@ -41,11 +22,8 @@ class AlarmClockApp implements EmulatorApplication {
     _emulator.screen.drawText(_emulator.getTime(), width / 2, height / 2 + 12,
         align: 'center');
 
-    if (_notifyTimer > 0) {
-      _emulator.screen.drawRect(0, 0, width, 20, style: 'purple');
-      _emulator.screen.drawText(_notifyText, 4, 12,
-          font: 'bold 12px Monospace', style: 'yellow');
-    }
+    _emulator.screen.drawRectInnerStroke(0, height - 40, width / 2, 40);
+    _emulator.screen.drawRectInnerStroke(width / 2, height - 40, width / 2, 40);
   }
 
   @override
